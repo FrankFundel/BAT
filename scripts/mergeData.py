d7043aa22a10ad44bf1e4b3cc771268015d3cc26
Frank <frank.fundel@outlook.com>
2021-12-13 22:41:16 +0100

    created mergeData

--- scripts/mergeData.py
+++ scripts/mergeData.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+import h5py
 
 df = pd.read_csv('E:/Chiroptera/Chiroptera.csv')
 
@@ -35,6 +36,38 @@ classes = {
   "Rhinolophus blasii": []
 }
 
+labels = {
+  "Rhinolophus ferrumequinum": 0,
+  "Rhinolophus hipposideros": 1,
+  "Myotis daubentonii": 2,
+  "Myotis brandtii": 3,
+  "Myotis mystacinus": 4,
+  "Myotis emarginatus": 5,
+  "Myotis nattereri": 6,
+  "Myotis bechsteinii": 7,
+  "Myotis myotis": 8,
+  "Nyctalus noctula": 9,
+  "Nyctalus leisleri": 10,
+  "Nyctalus lasiopterus": 11,
+  "Pipistrellus pipistrellus": 12,
+  "Pipistrellus pygmaeus": 13,
+  "Pipistrellus nathusii": 14,
+  "Pipistrellus kuhlii": 15,
+  "Hypsugo savii": 16,
+  "Vespertilio murinus": 17,
+  "Eptesicus serotinus": 18,
+  "Eptesicus nilssonii": 19,
+  "Plecotus auritus": 20,
+  "Plecotus austriacus": 21,
+  "Barbastella barbastellus": 22,
+  "Tadarida teniotis": 23,
+  "Miniopterus schreibersii": 24,
+  "Myotis capaccinii": 25,
+  "Myotis dasycneme": 26,
+  "Pipistrellus maderensis": 27,
+  "Rhinolophus blasii": 28
+}
+
 for index, row in df.iterrows():
   classes[row["species"]].append(row["filename"])
 
@@ -44,25 +77,87 @@ import librosa
 import numpy as np
 import soundfile as sf
 import os.path
+from keras.utils.np_utils import to_categorical
+from scipy import signal
+import cv2
+
+hop_length = 512
+sample_rate = 22050
+frame_rate = sample_rate / hop_length
+frequency_bins = int(1025 / 10)
+
+window_size = int(frame_rate / 2) # 500ms
+overlap = int(window_size / 2) # 250ms
+
+sequence_length = 30 # = 7.5 seconds (with overlap)
+sequence_overlap = int(sequence_length / 2)
+
+# results in shape X: (n, 11, 21, 102), Y: (n, 11)
+
+hf = h5py.File('data.h5', 'w')
+
+X_train = []
+Y_train = []
+
+X_test = []
+Y_test = []
+
+X_val = []
+Y_val = []
+
+b, a = signal.butter(10, 15000, 'highpass', analog=True)
+
+def denoise(x):
+  return np.abs(x - x.mean())
+
+def slideWindow(a, size, step):
+  b = []
+  i = 0
+  pos = 0
+  while pos + size < len(a):
+    pos = int(i  * step)
+    b.append(a[pos : pos + size])
+    i+=1
+  return b
+
+def prepare(y):
+    y, sr = librosa.load("E:/Chiroptera/Data/" + filename + '.wav')
+    filtered = signal.lfilter(b, a, y)
+    D = librosa.stft(filtered, hop_length=hop_length)
+    S_db = librosa.amplitude_to_db(np.abs(D), ref=np.max)  #spectrogram
+    S_db = np.apply_along_axis(denoise, axis=1, arr=S_db)
+    new_size = (frequency_bins, len(S_db[0]))
+    spectrogram = cv2.resize(S_db.transpose(), dsize=new_size, interpolation=cv2.INTER_NEAREST)
+    tiles = slideWindow(spectrogram, size=window_size, step=overlap)[:-1]
+    sequences = slideWindow(denoised_tiles, size=sequence_length, step=sequence_overlap)[:-1]
+    return sequences
 
 def mergeClass(name):
-  signals = []
+  files = []
   for filename in classes[name]:
-    y, sr = librosa.load("E:/Chiroptera/Data/" + filename + '.wav')
-    signals.append(y)
-  if len(signals) >= 7:
-    X_train, X_test, _, _ = train_test_split(signals, np.zeros(len(signals)), test_size=0.25, random_state=42)
-    X_train, X_val, _, _ = train_test_split(X_train, np.zeros(len(X_train)), test_size=0.2, random_state=42)
-    train = np.concatenate(X_train)
-    test = np.concatenate(X_test)
-    val = np.concatenate(X_val)
-    sf.write("E:/Chiroptera/train/" + name + '.wav', train, sr, 'PCM_24')
-    sf.write("E:/Chiroptera/test/" + name + '.wav', test, sr, 'PCM_24')
-    sf.write("E:/Chiroptera/val/" + name + '.wav', val, sr, 'PCM_24')
+    files.append(prepare(filename))
+  
+  label = to_categorical(labels[species], num_classes=len(labels)) # one hot
+  
+  if len(sequences) >= 7:
+    x_train, x_test, _, _ = train_test_split(files, np.zeros(len(files)), test_size=0.25, random_state=42)
+    x_train, x_val, _, _ = train_test_split(x_train, np.zeros(len(x_train)), test_size=0.2, random_state=42)
+    
+    X_train.extend(x_train)
+    Y_train.extend([label] * len(x_train))
+    X_test.extend(x_test)
+    Y_test.extend([label] * len(x_test))
+    X_val.extend(x_val)
+    Y_val.extend([label] * len(x_val))
   else:
-    print("no signal for " + name)
+    print("no sequences for " + name)
 
 for classname in list(classes):
-  if not os.path.exists("E:/Chiroptera/train/" + classname + '.wav') and not os.path.exists("E:/Chiroptera/test/" + classname + '.wav'):
-    mergeClass(classname)
-    print(classname + " merged!")
+  mergeClass(classname)
+  print(classname + " merged!")
+
+
+hf.create_dataset('train', data=[X_train, Y_train])
+hf.create_dataset('test', data=[X_test, Y_test])
+hf.create_dataset('val', data=[X_val, Y_val])
+hf.close()
---
+++ scripts/mergeSingals.py
@@ -0,0 +1,68 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+df = pd.read_csv('E:/Chiroptera/Chiroptera.csv')
+
+classes = {
+  "Rhinolophus ferrumequinum": [],
+  "Rhinolophus hipposideros": [],
+  "Myotis daubentonii": [],
+  "Myotis brandtii": [],
+  "Myotis mystacinus": [],
+  "Myotis emarginatus": [],
+  "Myotis nattereri": [],
+  "Myotis bechsteinii": [],
+  "Myotis myotis": [],
+  "Nyctalus noctula": [],
+  "Nyctalus leisleri": [],
+  "Nyctalus lasiopterus": [],
+  "Pipistrellus pipistrellus": [],
+  "Pipistrellus pygmaeus": [],
+  "Pipistrellus nathusii": [],
+  "Pipistrellus kuhlii": [],
+  "Hypsugo savii": [],
+  "Vespertilio murinus": [],
+  "Eptesicus serotinus": [],
+  "Eptesicus nilssonii": [],
+  "Plecotus auritus": [],
+  "Plecotus austriacus": [],
+  "Barbastella barbastellus": [],
+  "Tadarida teniotis": [],
+  "Miniopterus schreibersii": [],
+  "Myotis capaccinii": [],
+  "Myotis dasycneme": [],
+  "Pipistrellus maderensis": [],
+  "Rhinolophus blasii": []
+}
+
+for index, row in df.iterrows():
+  classes[row["species"]].append(row["filename"])
+
+print("sorted!")
+
+import librosa
+import numpy as np
+import soundfile as sf
+import os.path
+
+def mergeClass(name):
+  signals = []
+  for filename in classes[name]:
+    y, sr = librosa.load("E:/Chiroptera/Data/" + filename + '.wav')
+    signals.append(y)
+  if len(signals) >= 7:
+    X_train, X_test, _, _ = train_test_split(signals, np.zeros(len(signals)), test_size=0.25, random_state=42)
+    X_train, X_val, _, _ = train_test_split(X_train, np.zeros(len(X_train)), test_size=0.2, random_state=42)
+    train = np.concatenate(X_train)
+    test = np.concatenate(X_test)
+    val = np.concatenate(X_val)
+    sf.write("E:/Chiroptera/train/" + name + '.wav', train, sr, 'PCM_24')
+    sf.write("E:/Chiroptera/test/" + name + '.wav', test, sr, 'PCM_24')
+    sf.write("E:/Chiroptera/val/" + name + '.wav', val, sr, 'PCM_24')
+  else:
+    print("no signal for " + name)
+
+for classname in list(classes):
+  if not os.path.exists("E:/Chiroptera/train/" + classname + '.wav') and not os.path.exists("E:/Chiroptera/test/" + classname + '.wav'):
+    mergeClass(classname)
+    print(classname + " merged!")
