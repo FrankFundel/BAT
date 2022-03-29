6a357cbde4249e90e434c7d22a054f735143f2b5
Frank Fundel <frank.fundel@outlook.com>
2022-03-29 02:03:03 +0200

    redone merge.py

--- datasets/merge.py
+++ datasets/merge.py
@@ -1,7 +1,16 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+import librosa
+import numpy as np
+from scipy import signal
+from tqdm import tqdm
+import h5py
 
 df = pd.read_csv('../data.csv')
+hf = h5py.File('merged.h5', 'a')  # will be ~13GB
+train_set = hf.require_group("train")
+test_set = hf.require_group("test")
+val_set = hf.require_group("val")
 
 classes = {
   "Rhinolophus ferrumequinum": [],
@@ -40,29 +49,36 @@ for index, row in df.iterrows():
 
 print("sorted!")
 
-import librosa
-import numpy as np
-import soundfile as sf
-import os.path
+hop_length = 512
+
+b, a = signal.butter(10, 15000 / 120000, 'highpass')       # 1th order butterworth high-pass filter with cut-off frequency of 15,000 kHz
+
+def denoise(x):
+  return np.abs(x - x.mean())
 
 def mergeClass(name):
   signals = []
-  for filename in classes[name]:
-    y, sr = librosa.load("../Chiroptera/" + filename + '.wav')
-    signals.append(y)
+  for filename in tqdm(classes[name]):
+    y, _ = librosa.load("../data/" + filename + '.wav')
+    filtered = signal.lfilter(b, a, y)                      # filter
+    D = librosa.stft(filtered, hop_length=hop_length)
+    S_db = librosa.amplitude_to_db(np.abs(D), ref=np.max)   # spectrogram
+    S_db = np.apply_along_axis(denoise, axis=1, arr=S_db)   # denoise
+    signals.append(np.transpose(S_db))
+  
   if len(signals) >= 7:
     X_train, X_test, _, _ = train_test_split(signals, np.zeros(len(signals)), test_size=0.25, random_state=42)
     X_train, X_val, _, _ = train_test_split(X_train, np.zeros(len(X_train)), test_size=0.2, random_state=42)
     train = np.concatenate(X_train)
     test = np.concatenate(X_test)
     val = np.concatenate(X_val)
-    sf.write("../train/" + name + '.wav', train, sr, 'PCM_24')
-    sf.write("../test/" + name + '.wav', test, sr, 'PCM_24')
-    sf.write("../val/" + name + '.wav', val, sr, 'PCM_24')
-  else:
-    print("no signal for " + name)
+    train_set.create_dataset(name, data=train)
+    test_set.create_dataset(name, data=test)
+    val_set.create_dataset(name, data=val)
 
 for classname in list(classes):
-  if not os.path.exists("../train/" + classname + '.wav') and not os.path.exists("../test/" + classname + '.wav') and not os.path.exists("../val/" + classname + '.wav'):
+  if not classname in train_set or not classname in test_set or not classname in val_set:
     mergeClass(classname)
     print(classname + " merged!")
+
+hf.close()
--- datasets/prepare.py
+++ datasets/prepare.py
@@ -1,167 +1,18 @@
-import pandas as pd
-from sklearn.model_selection import train_test_split
-import h5py
+# open merged.h5
+# read arguments (s, w, sm)
 
-df = pd.read_csv('../data.csv')
+# create individuals_s.h5
+# create sequences_s.h5
+# for each set (train, test, val):
+#   for each classname:
+#     getIndividuals(S_db)
+#     getSequences(S_db)
 
-classes = {
-  "Rhinolophus ferrumequinum": [],
-  "Rhinolophus hipposideros": [],
-  "Myotis daubentonii": [],
-  "Myotis brandtii": [],
-  "Myotis mystacinus": [],
-  "Myotis emarginatus": [],
-  "Myotis nattereri": [],
-  "Myotis bechsteinii": [],
-  "Myotis myotis": [],
-  "Nyctalus noctula": [],
-  "Nyctalus leisleri": [],
-  "Nyctalus lasiopterus": [],
-  "Pipistrellus pipistrellus": [],
-  "Pipistrellus pygmaeus": [],
-  "Pipistrellus nathusii": [],
-  "Pipistrellus kuhlii": [],
-  "Hypsugo savii": [],
-  "Vespertilio murinus": [],
-  "Eptesicus serotinus": [],
-  "Eptesicus nilssonii": [],
-  "Plecotus auritus": [],
-  "Plecotus austriacus": [],
-  "Barbastella barbastellus": [],
-  "Tadarida teniotis": [],
-  "Miniopterus schreibersii": [],
-  "Myotis capaccinii": [],
-  "Myotis dasycneme": [],
-  "Pipistrellus maderensis": [],
-  "Rhinolophus blasii": []
-}
-
-labels = {
-  "Rhinolophus ferrumequinum": 0,
-  "Rhinolophus hipposideros": 1,
-  "Myotis daubentonii": 2,
-  "Myotis brandtii": 3,
-  "Myotis mystacinus": 4,
-  "Myotis emarginatus": 5,
-  "Myotis nattereri": 6,
-  "Myotis bechsteinii": 7,
-  "Myotis myotis": 8,
-  "Nyctalus noctula": 9,
-  "Nyctalus leisleri": 10,
-  "Nyctalus lasiopterus": 11,
-  "Pipistrellus pipistrellus": 12,
-  "Pipistrellus pygmaeus": 13,
-  "Pipistrellus nathusii": 14,
-  "Pipistrellus kuhlii": 15,
-  "Hypsugo savii": 16,
-  "Vespertilio murinus": 17,
-  "Eptesicus serotinus": 18,
-  "Eptesicus nilssonii": 19,
-  "Plecotus auritus": 20,
-  "Plecotus austriacus": 21,
-  "Barbastella barbastellus": 22,
-  "Tadarida teniotis": 23,
-  "Miniopterus schreibersii": 24,
-  "Myotis capaccinii": 25,
-  "Myotis dasycneme": 26,
-  "Pipistrellus maderensis": 27,
-  "Rhinolophus blasii": 28
-}
-
-for index, row in df.iterrows():
-  classes[row["species"]].append(row["filename"])
-
-print("sorted!")
-
-import librosa
-import numpy as np
-import soundfile as sf
-import os.path
-from keras.utils.np_utils import to_categorical
-from scipy import signal
-import cv2
-from tqdm import tqdm
-
-hop_length = 512
-sample_rate = 22050
-frame_rate = sample_rate / hop_length
-frequency_bins = int(1025 / 10)
-
-window_size = int(frame_rate / 2) # 500ms
-overlap = int(window_size / 2) # 250ms
-
-sequence_length = 15  # = 3.35 seconds (with overlap)
-sequence_overlap = int(sequence_length / 5)
-
-# results in shape X: (n, 11, 21, 102), Y: (n, 11)
-
-hf = h5py.File('data.h5', 'w')
-
-X_train = []
-Y_train = []
-
-X_test = []
-Y_test = []
-
-X_val = []
-Y_val = []
-
-b, a = signal.butter(10, 15000 / 120000, 'highpass')
-
-def denoise(x):
-  return np.abs(x - x.mean())
-
-def slideWindow(a, size, step):
-  b = []
-  i = 0
-  pos = 0
-  while pos + size < len(a):
-    pos = int(i  * step)
-    b.append(a[pos : pos + size])
-    i+=1
-  return b
-
-def prepare(filename):
-    y, _ = librosa.load("../Chiroptera/" + filename + '.wav', sample_rate)
-    filtered = signal.lfilter(b, a, y)
-    D = librosa.stft(filtered, hop_length=hop_length)
-    S_db = librosa.amplitude_to_db(np.abs(D), ref=np.max)  #spectrogram
-    S_db = np.apply_along_axis(denoise, axis=1, arr=S_db)
-    new_size = (frequency_bins, len(S_db[0]))
-    spectrogram = cv2.resize(S_db.transpose(), dsize=new_size, interpolation=cv2.INTER_NEAREST)
-    #tiles = slideWindow(spectrogram, size=window_size, step=overlap)[:-1]
-    #sequences = slideWindow(tiles, size=sequence_length, step=sequence_overlap)[:-1]
-    return spectrogram
-
-def mergeClass(name):
-  files = []
-  for filename in tqdm(classes[name]):
-    files.append(prepare(filename))
-  
-  label = to_categorical(labels[name], num_classes=len(labels)) # one hot
-  
-  if len(files) >= 7:
-    x_train, x_test, _, _ = train_test_split(files, np.zeros(len(files)), test_size=0.25, random_state=42)
-    x_train, x_val, _, _ = train_test_split(x_train, np.zeros(len(x_train)), test_size=0.2, random_state=42)
-    
-    #x_train_c = np.concatenate(x_train)
-    #x_test_c = np.concatenate(x_test)
-    #x_val_c = np.concatenate(x_val)
-
-    X_train.extend(x_train)
-    Y_train.extend([label] * len(x_train))
-    X_test.extend(x_test)
-    Y_test.extend([label] * len(x_test))
-    X_val.extend(x_val)
-    Y_val.extend([label] * len(x_val))
-
-for classname in tqdm(list(classes)):
-  mergeClass(classname)
-
-hf.create_dataset('X_train', data=X_train)
-hf.create_dataset('Y_train', data=Y_train)
-hf.create_dataset('X_test', data=X_test)
-hf.create_dataset('Y_test', data=Y_test)
-hf.create_dataset('X_val', data=X_val)
-hf.create_dataset('Y_val', data=Y_val)
-hf.close()
+# create individuals_w.h5
+# create sequences_w.h5
+# for each set (train, test, val):
+#   for each classname:
+#     S = librosa.db_to_amplitude(S_db, ref=np.max)
+#     y = librosa.griffinlim(S, hop_length=hop_length)
+#     getIndividuals(y)
+#     getSequences(y)
