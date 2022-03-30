0720f71e8df84c1a2c3887c438747ef0293947b6
Frank <frank.fundel@outlook.com>
2022-03-30 15:25:05 +0200

    fix prepare

--- datasets/prepare.py
+++ datasets/prepare.py
@@ -1,10 +1,10 @@
 import h5py
-import sys
 from tqdm import tqdm
 import librosa
 import numpy as np
 from keras.utils.np_utils import to_categorical
 from sklearn.utils import shuffle
+import cv2
 
 classes13 = {
   "Pipistrellus pipistrellus": 0,
@@ -53,16 +53,16 @@ labels = classes13
 
 merged_hf = h5py.File('merged.h5', 'r')
 
-mode = sys.argv[1]
-
 sample_rate = 22050                         # recordings are in 96 kHz, 24 bit depth, 1:10 TE (mic sr 960 kHz), 22050 Hz = 44100 Hz TE
-n_fft = 2048                                # 10 ms = 100 ms TE * sample_rate (e.g. 23 ms * 22050 Hz ~ 512)
-frame_rate = sample_rate / n_fft
+n_fft = 512                                 # 23 ms * 22050 Hz
+frame_rate = sample_rate / (n_fft // 4)	    # 22050 / 128 = 256
 
-window_size = int(frame_rate / 2)           # /2 = 500ms ~ 50ms
-overlap = int(window_size / 2)              # /2 = 250ms ~ 25ms
-sequence_length = 8                         # 15 = 3.35 seconds, 30 = 7.5 seconds (with overlap)
-sequence_overlap = int(sequence_length / 2)
+patch_len = 43           		    # = 250ms ~ 25ms
+patch_skip = 25			            # = 150ms ~ 15ms
+seq_length = 30			    	    # = 500ms with ~ 5 calls
+seq_skip = 15
+
+scale_factor = 1.0
 
 def slideWindow(a, size, step):
   b = []
@@ -75,21 +75,21 @@ def slideWindow(a, size, step):
   return b
 
 def getSequences(spectrogram):
-  tiles = slideWindow(spectrogram, size=window_size, step=overlap)[:-1] # last one is not full
-  sequences = slideWindow(tiles, size=sequence_length, step=sequence_overlap)[:-1] # last one is not full
+  tiles = slideWindow(spectrogram, size=path_len, step=patch_skip)[:-1] # last one is not full
+  sequences = slideWindow(tiles, size=seq_len, step=seq_skip)[:-1] # last one is not full
   return sequences
 
 def getIndividuals():
   # return sequences
   return
 
-if (mode == 's'):
-  seq_hf = h5py.File('sequences_s.h5', 'a')
-  #ind_hf = h5py.File('individuals_s.h5', 'a')
-  print("Preparing spectrogram dataset.")
 
   for set in ["train", "test", "val"]:
     merged_set = merged_hf.require_group(set)
+
+    seq_hf = h5py.File('sequences_s.h5', 'a')
+    #ind_hf = h5py.File('individuals_s.h5', 'a')
+
     X_seq = []
     Y_seq = []
     #X_ind = []
@@ -98,8 +98,11 @@ if (mode == 's'):
 
     for species in tqdm(list(labels)):
       S_db = merged_set.get(species)
+      new_size = S_db.shape * scale_factor
+      S_db = cv2.resize(np.float32(S_db), dsize=new_size, interpolation=cv2.INTER_NEAREST)
       label = to_categorical(labels[species], num_classes=len(labels)) # one hot encoding
       seq = getSequences(S_db)
+      print(S_db.shape)
       X_seq.extend(seq)
       Y_seq.extend([label] * len(seq))
 
@@ -107,35 +110,20 @@ if (mode == 's'):
       #X_ind.extend(ind)
       #Y_ind.extend([label] * len(ind))
 
+    print("Sequences:", len(X_seq))
+    #print("Individuals:", len(X_ind))
+
     print("Shuffling...")
     X_seq, Y_seq = shuffle(X_seq, Y_seq, random_state=42)
+
     print("Writing...")
     seq_hf.create_dataset('X_' + set, data=X_seq)
     seq_hf.create_dataset('Y_' + set, data=Y_seq)
+    seq_hf.close()
+    del X_seq
+    del Y_seq
+    
     #X_ind, Y_ind = shuffle(X_ind, Y_ind, random_state=42)
     #ind_hf.create_dataset('X_' + set, data=X_ind)
     #ind_hf.create_dataset('Y_' + set, data=Y_ind)
-  
-  seq_hf.close()
-  print("Sequences:", len(X_seq))
-  #ind_hf.close()
-  #print("Individuals:", len(X_ind))
-
-'''
-elif (mode == 'w'):
-  seq_hf = h5py.File('sequences_w.h5', 'a')
-  ind_hf = h5py.File('individuals_w.h5', 'a')
-
-  for set in ["train", "test", "val"]:
-    merged_set = merged_hf.require_group(set)
-    seq_set = seq_hf.require_group(set)
-    ind_set = ind_hf.require_group(set)
-    for species in tqdm(list(labels)):
-      S_db = merged_set.require_dataset(species)
-      S = librosa.db_to_amplitude(S_db, ref=np.max)
-      y = librosa.griffinlim(S, n_fft=n_fft)
-      seq_set.create_dataset(species, data=getSequences(y))
-      ind_set.create_dataset(species, data=getIndividuals(y))
-
-elif (mode == 'sm'):
-  print("TODO")'''
+    #ind_hf.close()
